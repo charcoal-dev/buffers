2934e9696e61e891fbabf29127afcf0e322654b3
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-12 07:51:45 +0500

    Simplify test headers and namespace/import declarations in test files.

--- tests/BufferManipulationTest.php
+++ tests/BufferManipulationTest.php
@@ -1,16 +1,12 @@
 <?php
-/*
- * This file is a part of "charcoal-dev/buffers" package.
- * https://github.com/charcoal-dev/buffers
- *
- * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code or visit following link:
- * https://github.com/charcoal-dev/buffers/blob/master/LICENSE
+/**
+ * Part of the "charcoal-dev/buffers" package.
+ * @link https://github.com/charcoal-dev/buffers
  */
 
 declare(strict_types=1);
+
+use Charcoal\Buffers\Buffer;
 
 /**
  * Class BufferManipulationTest
@@ -23,7 +19,7 @@ class BufferManipulationTest extends \PHPUnit\Framework\TestCase
      */
     public function testAsString(): void
     {
-        $buffer = \Charcoal\Buffers\Buffer::fromBase16("63686172636f616c");
+        $buffer = Buffer::fromBase16("63686172636f616c");
         $this->assertEquals(8, strlen((string)$buffer));
         $this->assertEquals("charcoal", strval($buffer));
     }
@@ -33,9 +29,9 @@ class BufferManipulationTest extends \PHPUnit\Framework\TestCase
      */
     public function testBufferFromMultipleCodes(): void
     {
-        $buffer = new \Charcoal\Buffers\Buffer(); // Blank buffer
-        $buffer->append(\Charcoal\Buffers\Buffer::fromBase16("63686172"));
-        $buffer->append(\Charcoal\Buffers\Buffer::fromByteArray([0x63, 0x6f, 0x61, 0x6c]));
+        $buffer = new Buffer(); // Blank buffer
+        $buffer->append(Buffer::fromBase16("63686172"));
+        $buffer->append(Buffer::fromByteArray([0x63, 0x6f, 0x61, 0x6c]));
         $this->assertEquals("charcoal", $buffer->raw());
     }
 
@@ -44,7 +40,7 @@ class BufferManipulationTest extends \PHPUnit\Framework\TestCase
      */
     public function testPopMethod(): void
     {
-        $buffer = new \Charcoal\Buffers\Buffer("charcoal");
+        $buffer = new Buffer("charcoal");
         $this->assertEquals("ch", $buffer->pop(2, changeBuffer: false)); // Pop 2 bytes, don't change buffer
         $this->assertEquals("charcoal", $buffer->raw()); // Buffer was not altered
         $this->assertEquals("char", $buffer->pop(4, changeBuffer: true)); // Pop first 4 bytes, CHANGE the buffer internally
@@ -60,7 +56,7 @@ class BufferManipulationTest extends \PHPUnit\Framework\TestCase
      */
     public function testCopyAndEqualMethods(): void
     {
-        $buffer1 = new \Charcoal\Buffers\Buffer("charcoal");
+        $buffer1 = new Buffer("charcoal");
 
         $this->assertEquals("charcoal", $buffer1->copy()->raw(), "Simple Copy");
         $this->assertEquals("char", $buffer1->copy(0, 4)->raw(), "Copy initial 4 bytes as new Buffer");
@@ -70,9 +66,9 @@ class BufferManipulationTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals("coa", $buffer1->copy(-4, 3)->raw(), "Copy 3 bytes from set of last 4 bytes");
         $this->assertEquals("arcoal", $buffer1->copy(2)->raw(), "Copy all bytes as new Buffer except first 2");
 
-        $coal = new \Charcoal\Buffers\Buffer("coal");
+        $coal = new Buffer("coal");
         $this->assertTrue($buffer1->copy(-4)->equals($coal));
-        $this->assertFalse($buffer1->copy(-4)->equals(new \Charcoal\Buffers\Buffer("cola")));
+        $this->assertFalse($buffer1->copy(-4)->equals(new Buffer("cola")));
     }
 
     /**
@@ -80,7 +76,7 @@ class BufferManipulationTest extends \PHPUnit\Framework\TestCase
      */
     public function testSwitchEndiannessMethod(): void
     {
-        $buffer = \Charcoal\Buffers\Buffer::fromBase16("a1b2c3");
+        $buffer = Buffer::fromBase16("a1b2c3");
         $switched = $buffer->switchEndianness();
         $this->assertEquals("c3b2a1", $switched->toBase16());
     }
@@ -90,7 +86,7 @@ class BufferManipulationTest extends \PHPUnit\Framework\TestCase
      */
     public function testApplyFn(): void
     {
-        $buffer = new Charcoal\Buffers\Buffer(" charcoal\t\0");
+        $buffer = new Buffer(" charcoal\t\0");
         $manipulated = $buffer->applyFn(function (string $bytes) {
             return strtolower(trim($bytes)) . ".dev";
         });
