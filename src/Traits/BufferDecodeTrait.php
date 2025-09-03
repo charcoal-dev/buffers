4f154711f46884415f96dcffd6aaba0bb0121a4f
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-03 17:30:36 +0500

    Enhance `BufferDecodeTrait` with encoding scheme interface support and add `fromBase16` and `fromBase64` decoding methods.

--- src/Traits/BufferDecodeTrait.php
+++ src/Traits/BufferDecodeTrait.php
@@ -10,6 +10,7 @@ namespace Charcoal\Buffers\Traits;
 
 use Charcoal\Buffers\Enums\BufferEncoding;
 use Charcoal\Contracts\Buffers\ByteArrayInterface;
+use Charcoal\Contracts\Encoding\EncodingSchemeInterface;
 
 /**
  * Provides functionality related to decoding buffer data
@@ -18,12 +19,26 @@ use Charcoal\Contracts\Buffers\ByteArrayInterface;
 trait BufferDecodeTrait
 {
     /**
-     * @param BufferEncoding $scheme
-     * @param string $data
-     * @return static
+     *  Instantiate buffer from encoded data
      */
-    public static function decode(BufferEncoding $scheme, string $data): static
+    public static function decode(EncodingSchemeInterface $scheme, string $data): static
     {
         return new static($scheme->decode($data));
+    }
+
+    /**
+     * Instantiate buffer from Base64 encoded data
+     */
+    public static function fromBase16(string $data): static
+    {
+        return new static(BufferEncoding::decodeBase16($data));
+    }
+
+    /**
+     * Instantiate buffer from Base64 encoded data
+     */
+    public static function fromBase64(string $data): static
+    {
+        return new static(BufferEncoding::decodeBase64($data));
     }
 }
