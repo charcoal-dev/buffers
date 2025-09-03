26a91b58b6c8ceeca79e45081e27907f5442535f
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-03 18:10:29 +0500

    Add `BufferImmutable` class for immutable buffer handling with appending, prepending, and substring support

---
+++ src/BufferImmutable.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Part of the "charcoal-dev/buffers" package.
+ * @link https://github.com/charcoal-dev/buffers
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Buffers;
+
+use Charcoal\Buffers\Traits\BufferDecodeTrait;
+use Charcoal\Buffers\Traits\ReadableBufferTrait;
+use Charcoal\Contracts\Buffers\BufferSpinOffInterface;
+use Charcoal\Contracts\Buffers\ImmutableBufferInterface;
+use Charcoal\Contracts\Buffers\ReadableBufferInterface;
+
+/**
+ * Represents an immutable buffer of bytes. This class provides methods
+ * for creating modified copies of the buffer with data appended or prepended,
+ * without altering the original instance. It also supports accessing substrings
+ * of the buffer's data.
+ */
+readonly class BufferImmutable implements
+    ImmutableBufferInterface,
+    ReadableBufferInterface,
+    BufferSpinOffInterface
+{
+    use ReadableBufferTrait;
+    use BufferDecodeTrait;
+
+    private int $length;
+
+    public function __construct(private string $bytes)
+    {
+        $this->length = strlen($this->bytes);
+    }
+
+    /**
+     * Returns a new instance with data appended.
+     * @param ReadableBufferInterface|string $data
+     * @return BufferImmutable
+     */
+    public function withAppended(ReadableBufferInterface|string $data): static
+    {
+        return new BufferImmutable($this->bytes .
+            ($data instanceof ReadableBufferInterface ? $data->bytes() : $data));
+    }
+
+    /**
+     * Returns a new instance with data prepended.
+     * @param ReadableBufferInterface|string $data
+     * @return BufferImmutable
+     */
+    public function withPrepended(ReadableBufferInterface|string $data): static
+    {
+        return new BufferImmutable(($data instanceof ReadableBufferInterface ? $data->bytes() : $data)
+            . $this->bytes);
+    }
+
+    /**
+     * Returns resulting starting after applying offset and length.
+     */
+    public function substr(int $offset, int $length = null): string
+    {
+        return $this->_substr($this->length, $offset, $length);
+    }
+}
